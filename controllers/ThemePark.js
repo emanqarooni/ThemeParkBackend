f36de7ae78a7098a83e22692b6274a6e0c96951b
mohamed-janahi <Mohamed.janahi98@gmail.com>
2025-10-21 11:52:45 +0300

    added the get and create and delete controllers and routes

---
+++ controllers/ThemePark.js
@@ -0,0 +1,33 @@
+const {Post} = require("../models/themePark")
+
+const GetPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({})
+    res.status(200).send(posts)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreatePost = async (req, res) => {
+  try {
+    const post = await Post.create(req.body)
+    res.status(200).send(post)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeletePost = async (req, res) => {
+  try {
+    await Post.deleteOne({ _id: req.params.id })
+    res.status(200).send({ msg: 'Post Deleted', id: req.params.id })
+  } catch (error) {
+    throw error
+  }
+}
+module.exports = {
+  GetPosts,
+  CreatePost,
+  DeletePost,
+}
