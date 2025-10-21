c29f6552d8c67fdaa8a9dd72d61bacb4481f6ad5
emanqarooni <emanqaroonibahrain@gmail.com>
2025-10-21 17:51:41 +0300

    updating

--- controllers/ThemePark.js
+++ controllers/ThemePark.js
@@ -1,30 +1,40 @@
 const axios = require("axios")
-const themePark = require("../models/themePark")
+const ThemePark = require("../models/themePark")
+
+//get all theme parks
 const GetThemePark = async (req, res) => {
   try {
-    const themePark = await themePark.find({})
-    res.status(200).send(themePark)
+    const parks = await ThemePark.find({})
+    res.status(200).send(parks)
   } catch (error) {
-    throw error
-  }
-}
-const CreateThemePark = async (req, res) => {
-  try {
-    const themePark = await themePark.create(req.body)
-    res.status(200).send(themePark)
-  } catch (error) {
-    throw error
-  }
-}
-const DeleteThemePark = async (req, res) => {
-  try {
-    await themePark.deleteOne({ _id: req.params.id })
-    res.status(200).send({ msg: "ThemePark Deleted", id: req.params.id })
-  } catch (error) {
-    throw error
+    console.error("Error fetching theme parks:", error)
+    res.status(500).send("Error fetching theme parks")
   }
 }
 
+//create a new theme park
+const CreateThemePark = async (req, res) => {
+  try {
+    const newPark = await ThemePark.create(req.body)
+    res.status(201).send(newPark)
+  } catch (error) {
+    console.error("Error creating theme park:", error)
+    res.status(500).send("Error creating theme park")
+  }
+}
+
+//delete a theme park
+const DeleteThemePark = async (req, res) => {
+  try {
+    await ThemePark.deleteOne({ _id: req.params.id })
+    res.status(200).send({ msg: "ThemePark Deleted", id: req.params.id })
+  } catch (error) {
+    console.error("Error deleting theme park:", error)
+    res.status(500).send("Error deleting theme park")
+  }
+}
+
+//get games from external Park Fan API
 const GetGames = async (req, res) => {
   try {
     const response = await axios.get("https://api.park.fan/games")
