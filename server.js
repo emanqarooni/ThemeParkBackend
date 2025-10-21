ad78d12c917ab9abe664b6486ca0fd6123a77787
emanqarooni <emanqaroonibahrain@gmail.com>
2025-10-21 09:35:03 +0300

    setup done

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+
+const logger = require("morgan")
+const dotenv = require("dotenv")
+
+const PORT = process.env.PORT || 3000
+
+const db = require("./config/db")
+
+//use app
+app.use(logger("dev"))
+dotenv.config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use("/", (req, res) => {
+  res.send(`Connected!`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Running Express server on Port ${PORT} . . .`)
+})
