a232ea02ab05a4a235b1858fdf8a1bf4657b3c60
emanqarooni <emanqaroonibahrain@gmail.com>
2025-10-21 09:47:42 +0300

    create model

---
+++ models/themePark.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+
+//the schema is from the mongoose library
+const themeParkSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    timing: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true, //createdAt, updatedAt: if i want to create a record, you should be logging the when data is created and what time it is updated
+  }
+)
+
+//creating the model then connect it with the schema
+const themePark = mongoose.model("themePark", themeParkSchema)
+
+//export the model
+module.exports = themePark
